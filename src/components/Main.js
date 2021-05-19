89468b11238f39322e6fa974321d228d4ab80f46
Chopardissmo <gel@alumni.usc.edu>
2021-05-19 17:28:31 +0800

    implement fetch data from API and display info

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ export default class Main extends Component {
         this.state = {
             satInfo: null,
             settings: null,
+            loadingSatellites: false,
         };
     }
 
@@ -24,23 +25,36 @@ export default class Main extends Component {
         const { observerLat, observerLong, observerAlt, radius } = setting;
         const url = `/api${NEARBY_SATELLITE}/${observerLat}/${observerLong}/${observerAlt}/${radius}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
 
+        // before send the request, display loading animation
+        this.setState({
+            loadingSatellites: true,
+        });
+
         Axios.get(url)
             .then((response) => {
                 this.setState({
                     satInfo: response.data,
+                    loadingSatellites: false,
                 });
             })
             .catch((error) => {
                 console.log("err in fetch satellite -> ", error);
+                this.setState({
+                    loadingSatellites: false,
+                });
             });
     };
 
     render() {
+        const { satInfo, loadingSatellites } = this.state;
         return (
             <div className="main">
                 <div className="left-side">
                     <SatSetting onShow={this.showNearbySatellite} />
-                    <SatelliteList satInfo={this.state.satInfo} />
+                    <SatelliteList
+                        satInfo={satInfo}
+                        loading={loadingSatellites}
+                    />
                 </div>
                 <div className="right-side">right</div>
             </div>
--- src/components/SatelliteList.js
+++ src/components/SatelliteList.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { List, Avatar, Button, Checkbox } from "antd";
+import { List, Avatar, Button, Checkbox, Spin } from "antd";
 import Satellite from "../assets/image/satellite.svg";
 
 export default class SatelliteList extends Component {
     render() {
-        const satList = this.props.satInfo ? this.props.satInfo.above : [];
+        const { satInfo, loading } = this.props;
+        const satList = satInfo ? satInfo.above : [];
 
         return (
             <div className="sat-list-box">
@@ -14,28 +15,34 @@ export default class SatelliteList extends Component {
 
                 <hr />
 
-                <List
-                    className="sat-list"
-                    itemLayout="horizontal"
-                    size="small"
-                    dataSource={satList}
-                    renderItem={(item) => (
-                        <List.Item
-                            actions={[
-                                <Checkbox
-                                    dataInfo={item}
-                                    onChange={this.onChange}
-                                />,
-                            ]}
-                        >
-                            <List.Item.Meta
-                                avatar={<Avatar size={50} src={Satellite} />}
-                                title={<p>{item.satname}</p>}
-                                description={`Launch Date: ${item.launchDate}`}
-                            />
-                        </List.Item>
-                    )}
-                />
+                {loading ? (
+                    <Spin tip="Loading Satellites..." />
+                ) : (
+                    <List
+                        className="sat-list"
+                        itemLayout="horizontal"
+                        size="small"
+                        dataSource={satList}
+                        renderItem={(item) => (
+                            <List.Item
+                                actions={[
+                                    <Checkbox
+                                        dataInfo={item}
+                                        onChange={this.onChange}
+                                    />,
+                                ]}
+                            >
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar size={50} src={Satellite} />
+                                    }
+                                    title={<p>{item.satname}</p>}
+                                    description={`Launch Date: ${item.launchDate}`}
+                                />
+                            </List.Item>
+                        )}
+                    />
+                )}
             </div>
         );
     }
