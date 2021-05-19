0e45e3b6564618af219830ef108c014d5ff6191d
Chopardissmo <gel@alumni.usc.edu>
2021-05-19 18:02:33 +0800

    develop map generation feature when SPA initialize

--- src/components/WorldMap.js
+++ src/components/WorldMap.js
@@ -1,7 +1,81 @@
 import React, { Component } from "react";
+import { feature } from "topojson-client";
+import axios from "axios";
+import { geoKavrayskiy7 } from "d3-geo-projection";
+import { geoGraticule, geoPath } from "d3-geo";
+import { select as d3Select } from "d3-selection";
+
+import { WORLD_MAP_URL } from "../constant";
+
+const width = 960;
+const height = 600;
 
 export default class WorldMap extends Component {
+    constructor() {
+        super();
+        this.state = {
+            map: null,
+        };
+        this.refMap = React.createRef();
+    }
+
+    componentDidMount() {
+        axios
+            .get(WORLD_MAP_URL)
+            .then((res) => {
+                const { data } = res;
+                const land = feature(data, data.objects.countries).features;
+                this.generateMap(land);
+            })
+            .catch((e) => console.log("err in fecth world map data ", e));
+    }
+
+    generateMap(land) {
+        const projection = geoKavrayskiy7()
+            .scale(170)
+            .translate([width / 2, height / 2])
+            .precision(0.1);
+
+        const graticule = geoGraticule();
+
+        const canvas = d3Select(this.refMap.current)
+            .attr("width", width)
+            .attr("height", height);
+
+        let context = canvas.node().getContext("2d");
+
+        let path = geoPath().projection(projection).context(context);
+
+        land.forEach((ele) => {
+            // draw the countries
+            context.fillStyle = "#B3DDEF";
+            context.strokeStyle = "#000";
+            context.globalAlpha = 0.7;
+            context.beginPath();
+            path(ele);
+            context.fill();
+            context.stroke();
+
+            // draw the graticule
+            context.strokeStyle = "rgba(220, 220, 220, 0.1)";
+            context.beginPath();
+            path(graticule());
+            context.lineWidth = 0.1;
+            context.stroke();
+
+            // draw the graticule outline
+            context.beginPath();
+            context.lineWidth = 0.5;
+            path(graticule.outline());
+            context.stroke();
+        });
+    }
+
     render() {
-        return <div>world map</div>;
+        return (
+            <div className="map-box">
+                <canvas className="map" ref={this.refMap} />
+            </div>
+        );
     }
 }
